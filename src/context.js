b6858dfffe6b4c2566ab755b24d14bb4606e107e
sorawit <slert@sertiscorp.com>
2020-11-19 17:35:28 +0700

    config eslint

--- src/context.js
+++ src/context.js
@@ -1,7 +1,6 @@
 import glob from 'glob'
 
 export async function context({ req }) {
-  console.log()
   // get all models
   const importModels = glob.sync(`${__dirname}/domain/*/model`).map(path => require(path))
   const model = importModels.reduce((result, obj) => ({ ...result, ...obj }) , {})
