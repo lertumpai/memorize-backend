fadc43389a1c318f31dba308b40f0d242beaea29
ninew <new-sorawit@outlook.com>
2020-11-22 16:14:14 +0700

    prepare function list index

--- src/database/mongo/resolvers.js
+++ src/database/mongo/resolvers.js
@@ -1,7 +1,23 @@
+import { models } from 'mongoose'
+
+async function listIndexes() {
+  const indexes = await Promise.all(
+    Object
+      .entries(models)
+      .map(async ([key, model]) => {
+        const indexes = await model.listIndexes()
+        return indexes.map(index => `${key}: ${index.name}`)
+      })
+  )
+  return indexes.flat()
+}
+
 module.exports = {
   Mutation: {
-    syncMongoIndexes() {
-      return 'finish'
+    async syncMongoIndexes() {
+      const indexesBeforeSync = await listIndexes()
+      const indexesAfterSync = await listIndexes()
+      return 'hi'
     },
   },
 }
