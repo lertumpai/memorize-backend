480189233b089bbc33caaf89d54211628a1b86e9
ninew <new-sorawit@outlook.com>
2020-11-24 00:39:55 +0700

    pagination

--- src/database/mongo/dao.js
+++ src/database/mongo/dao.js
@@ -7,6 +7,17 @@ export default class Dao {
     return this.model.findById(id)
   }
 
+  queryAfterBeforeLimit(filer, { after, before, limit = 10, sortBy = '_id', order = 'DESC' }) {
+    let pagination
+    if (!!after && !!before) {
+      pagination = { $gte: after, $lte: before }
+    } else if (after) {
+      pagination = { $gt: after }
+    } else if (before) {
+      pagination = { $lt: before }
+    }
+  }
+
   deleteById(id) {
     return this.model.findOneAndUpdate(id, { active: false }, { new: true })
   }
