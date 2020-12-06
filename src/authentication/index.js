ae65b8b9247fa52dc84a405a67841bbbe10e05aa
ninew <new-sorawit@outlook.com>
2020-12-06 20:33:26 +0700

    refactor error message

--- src/authentication/index.js
+++ src/authentication/index.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt'
 
-import { DUPLICATED_VALUE_ERROR, LOGIN_FAIL_ERROR, REGISTER_FAIL_ERROR } from '../error'
+import { DUPLICATED_VALUE_ERROR, LOGIN_FAIL_ERROR, REGISTER_FAIL_ERROR, NOT_FOUND_ERROR } from '../error'
 
 export async function register({ username, password }, { User, date }) {
   const checks = []
@@ -19,9 +19,14 @@ export async function register({ username, password }, { User, date }) {
 }
 
 export async function login({ username, password }, { User }) {
+  const checks = []
+  if (!username) checks.push('username')
+  if (!password) checks.push('password')
+  if (checks.length !== 0) throw new LOGIN_FAIL_ERROR(checks)
+
   const user = await User.findByUsername(username)
 
-  if (!user) throw new LOGIN_FAIL_ERROR(['username'])
+  if (!user) throw new NOT_FOUND_ERROR('username')
   if (!password || !bcrypt.compareSync(password, user.password)) throw new LOGIN_FAIL_ERROR(['password'])
 
   return user
