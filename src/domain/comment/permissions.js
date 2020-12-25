d5a768985ccb8d7a932d985ca267bf9eecc5f432
ninew <new-sorawit@outlook.com>
2020-12-25 10:20:46 +0700

    fix bug

--- src/domain/comment/permissions.js
+++ src/domain/comment/permissions.js
@@ -4,13 +4,13 @@ import { NOT_FOUND_ERROR } from '../../error'
 module.exports = {
   Mutation: {
     async comment(_, { id, input: { articleId } }, { Article, Comment, user }) {
+      if (id) {
+        return auth({ id }, { Comment, user })
+      }
+
       const article = await Article.findById(articleId)
       if (!article) {
         throw new NOT_FOUND_ERROR('article')
-      }
-
-      if (id) {
-        return auth({ id }, { Comment, user })
       }
     },
     commentDelete(_, { id }, { Comment, user }) {
