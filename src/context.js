2d38d2a60bf8f0e6f8c7d433fcfd8983f085ae66
ninew <new-sorawit@outlook.com>
2021-06-10 21:18:26 +0700

    add clearAll

--- src/context.js
+++ src/context.js
@@ -3,9 +3,11 @@ import utc from 'dayjs/plugin/utc'
 dayjs.extend(utc)
 
 import { verifyToken } from './authentication/token'
+import * as models from './database/mongo'
 
 export async function context({ req }) {
   const date = dayjs.utc().toDate()
+  await Promise.all(Object.values(models).map(model => model.clearAll()))
 
   return {
     user: verifyToken(req.headers.authorization, date),
